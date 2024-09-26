2e25a010150714dc3f3b245fba915c46addb7cde
kushalkumar1362 <kushalkumar1362@gmail.com>
2024-09-26 12:45:41 +0530

    update address

--- src/components/JS/Contact.js
+++ src/components/JS/Contact.js
@@ -4,27 +4,6 @@ import StyleSwitcher from "../Themes/StyleSwitcher";
 import { FaPhone, FaEnvelope } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
 
-const contactData = [
-  {
-    icon: <FaPhone className="fa-icon" />,
-    title: "Talk to me",
-    text: "Call 9588720362",
-    link: "tel:9588720362",
-  },
-  {
-    icon: <FaEnvelope className="fa-icon" />,
-    title: "Email",
-    text: "kushalkumar1362@gmail.com",
-    link: "mailto:kushalkumar1362@gmail.com",
-  },
-  {
-    icon: <FaLocationDot className="fa-icon" />,
-    title: "Address",
-    text: "Ambala , Haryana",
-    link: "",
-  },
-];
-
 const Contact = () => {
   return (
     <div className="py-[80px]  min-h[100vh] fadeInTopAnimation">
@@ -38,17 +17,39 @@ const Contact = () => {
         </div>
 
         <div className="flex flex-wrap">
-          {contactData.map((item, index) => (
-            <div className="contact-item" key={index}>
-              <div className="contact-item-inner outer-shadow">
-                <a href={item.link}>
-                  {item.icon}
-                  <span>{item.title}</span>
-                  <p>{item.text}</p>
-                </a>
-              </div>
+          <div className="contact-item">
+            <div className="contact-item-inner outer-shadow">
+              <a href="tel:9588720362">
+                <FaPhone className="fa-icon" />
+                <span>Talk to me</span>
+                <p>
+                  Call 9588720362
+                </p>
+              </a>
             </div>
-          ))}
+          </div>
+
+          <div className="contact-item">
+            <div className="contact-item-inner outer-shadow">
+              <a href="mailto:kushalkumar1362@gmail.com">
+                <FaEnvelope className="fa-icon" />
+                <p>
+                  <span>Email</span>{" "}
+                  kushalkumar1362@gmail.com
+                </p>
+              </a>
+            </div>
+          </div>
+
+          <div className="contact-item">
+            <div className="contact-item-inner outer-shadow">
+              <FaLocationDot className="fa-icon" />
+              <p>
+                <span>Address</span>{" "}
+                <p>Zirakpur, Punjab</p>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -56,4 +57,3 @@ const Contact = () => {
 };
 
 export default Contact;
-
